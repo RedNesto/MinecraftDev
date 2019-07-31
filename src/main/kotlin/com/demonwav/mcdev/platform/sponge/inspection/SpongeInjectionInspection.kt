9565908d479431745e957f4f3fb327dee3a74b3b
RedNesto <RedNestoMC@gmail.com>
2019-07-31 09:19:46 +0200

    Add license headers

--- src/main/kotlin/com/demonwav/mcdev/platform/sponge/inspection/SpongeInjectionInspection.kt
+++ src/main/kotlin/com/demonwav/mcdev/platform/sponge/inspection/SpongeInjectionInspection.kt
@@ -1,3 +1,13 @@
+/*
+ * Minecraft Dev for IntelliJ
+ *
+ * https://minecraftdev.org
+ *
+ * Copyright (c) 2019 minecraft-dev
+ *
+ * MIT License
+ */
+
 package com.demonwav.mcdev.platform.sponge.inspection
 
 import com.demonwav.mcdev.platform.sponge.SpongeModuleType
